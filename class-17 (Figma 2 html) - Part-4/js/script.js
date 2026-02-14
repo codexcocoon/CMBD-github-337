83cf62cc7d6984c1ab2f743a3a915933030f77d1
codexcocoon <codexcocoon@gmail.com>
2026-02-14 15:34:20 +0600

    figma 2 html with responsive complete

---
+++ class-17 (Figma 2 html) - Part-4/js/script.js
@@ -0,0 +1,5 @@
+const mbi = document.getElementById("mbi");
+const navLinks = document.getElementById("nav-links");
+mbi.addEventListener("click", () => {
+  navLinks.classList.toggle('active');
+});
